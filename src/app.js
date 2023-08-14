9818d0fba2a049f264f580f9b8587e41ac928c93
Piotr <lewczykpiotr@gmail.com>
2023-08-14 13:45:23 +0200

    ability to paste AST object into textarea and convert it to html string

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const inputMessage = document.querySelector('.input');
 const outputMessage = document.querySelector('.output');
 const button = document.querySelector('#button')
 
+inputMessage.value = ''
+
 inputMessage.addEventListener('input', (e) => {
   if (e.currentTarget.value === '') {
     outputMessage.classList.add('output-visibility')
@@ -16,7 +18,13 @@ function showMessage(e) {
   const textareaValue = inputMessage.value
   if (!textareaValue) return;
 
-  const convertedString = convertASTToString(textareaValue);
   outputMessage.classList.remove('output-visibility')
-  outputMessage.textContent = convertedString;
+
+  try {
+    const { ast: astObject } = JSON.parse(textareaValue)
+    const htmlString = convertASTToString(astObject)
+    outputMessage.textContent = htmlString
+  } catch (err) {
+    outputMessage.textContent = err.message;
+  }
 }
--- src/modules/convertASTToString.js
+++ src/modules/convertASTToString.js
@@ -1,11 +1,43 @@
-function convertASTToString(input) {
-  checkInput(input);
-  return 'blabla'
+function convertASTToString(astObject) {
+  let htmlString = ''
+
+  if (astObject.nodeType === 'element') {
+    htmlString += `<${astObject.tagName}`
+
+    if (astObject.attributes) {
+      astObject.attributes.forEach(attr => {
+        htmlString += ` ${attr.name}="${attr.value}"`
+      })
+    }
+
+    htmlString += '>'
+
+    if (astObject.children) {
+      astObject.children.forEach(child => {
+        htmlString += convertASTToString(child)
+      })
+    }
+
+    console.log(astObject)
+    htmlString += closeTag(astObject.tagName)
+
+  } else if (astObject.nodeType === 'text') {
+    htmlString += astObject.value
+  }
+
+  return htmlString
 }
 
-function checkInput(input) {
-  if (input === '') throw new Error('Input can not be empty');
-  if (typeof input !== 'string') throw new Error('Input must be a string');
+function closeTag(tag) {
+  const tagsForbiddenToBeClosed = [
+    'img', 'input', 'br', 'hr'
+  ]
+
+  if (tagsForbiddenToBeClosed.includes(tag)) {
+    return ''
+  }
+
+  return `</${tag}>`
 }
 
 export default convertASTToString
